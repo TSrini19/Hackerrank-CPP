22bfbd3dc2e71fee38d80ec0dbfd24681a5012a7
Taarun Srinivas <104182172+TSrini19@users.noreply.github.com>
2022-05-31 20:03:01 +0530

    basic data types

---
+++ basic_data_types.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include "bits/stdc++.h"
+//using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    
+    std::cin >> a >> std::ws >> b >> std::ws >> c >> std::ws >> d >> std::ws >> e;
+    std::cout << a << std::endl; 
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << std::fixed << std::setprecision(3) << d << std::endl;
+    std::cout << std::fixed << std::setprecision(9) << e << std::endl;
+    return 0;
+}
